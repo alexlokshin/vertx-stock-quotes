4261ea74c17d60d7f71fa2c7bd7207dee45ccf07
Alex Lokshin <alokshin@gmail.com>
2019-02-16 11:54:14 -0500

    Vulnerabilities?

--- src/test/java/com/avsatum/codegen/TestMainVerticle.java
+++ src/test/java/com/avsatum/codegen/TestMainVerticle.java
@@ -25,10 +25,10 @@ public class TestMainVerticle {
 	@DisplayName("Should start a Web Server on port 8080")
 	@Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
 	void start_http_server(Vertx vertx, VertxTestContext testContext) throws Throwable {
-		vertx.createHttpClient().getNow(8080, "localhost", "/", response -> testContext.verify(() -> {
+		vertx.createHttpClient().getNow(8080, "localhost", "/api/aapl", response -> testContext.verify(() -> {
 			assertTrue(response.statusCode() == 200);
 			response.handler(body -> {
-				assertTrue(body.toString().contains("Hello from Vert.x!"));
+				assertTrue(body.toString().contains("StockQuote"));
 				testContext.completeNow();
 			});
 		}));
